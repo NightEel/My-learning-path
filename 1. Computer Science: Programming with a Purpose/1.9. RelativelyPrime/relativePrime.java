75d65117472f174756ce33efead4e105dec2118c
The Night Eel <110661010+NightEel@users.noreply.github.com>
2022-08-07 22:48:07 +0200

    Adding some comentaries

--- 1. Computer Science: Programming with a Purpose/1.9. RelativelyPrime/relativePrime.java
+++ 1. Computer Science: Programming with a Purpose/1.9. RelativelyPrime/relativePrime.java
@@ -1,16 +1,20 @@
 public class relativePrime {
     public static void main(String[] args)
     {
+        //Saving the input
         int n   =   Integer.parseInt(args[0]);
+        
+        //Printing the table
         for (long i = 1; i <= n; i++)
         {
-            long    b   =   i;
+            long    b   =   i;                      //Necessary because i will change during the calculation of the gcd
             for (long j = 1; j <= n; j++)
             {
-                long a      =   j;
-                double k    =   1;
+                long a      =   j;                  //Necessary because j will change during the calculation of the gcd
+                double k    =   1;                  //Because 1 is the multiplicative identity in the ring of integers
+                
                 //Finding the gcd
-                if (i == 1 || j == 1) k   =   1;
+                if (i == 1 || j == 1) k   =   1;    //I know that k = 1 is redundant but I don't know any other way to do nothing yet
                 else
                 {
                     long factor =   2;
@@ -21,7 +25,6 @@ public class relativePrime {
                             i   =   i / factor;
                             if (j % factor == 0)
                             {
-                                //System.out.print(factor + " ");
                                 j   =   j / factor;
                                 k   =   k * factor;
                             }
@@ -30,6 +33,8 @@ public class relativePrime {
                     }
                 }
                 if (i > 1 && j % i == 0) k  =   k * i;
+                //Here ends the calculation of the gcd
+                
                 j   =   a;
                 i   =   b;
                 if (k == 1) System.out.print(" * ");
