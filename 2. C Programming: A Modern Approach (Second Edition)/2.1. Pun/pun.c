93a6af5e131efe95dbd03891db1c91ace75c5ccf
The Night Eel <110661010+NightEel@users.noreply.github.com>
2022-08-08 13:30:34 +0200

    Adding comments to pun.c

---
+++ 2. C Programming: A Modern Approach (Second Edition)/2.1. Pun/pun.c
@@ -0,0 +1,11 @@
+// Name: pun.c
+// Purpose: Prints a bad pun.
+// Author: The Night Eel
+
+#include <stdio.h>
+
+int main(void)                                          // Beginning of main program
+{
+  printf("To C, or not to C: that is the question. \n");
+  return 0;
+}
