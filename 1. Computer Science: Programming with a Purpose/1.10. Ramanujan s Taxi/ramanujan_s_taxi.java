f69fc2daed1d61ede0f5c5ee0b85baa391d8cb37
The Night Eel <110661010+NightEel@users.noreply.github.com>
2022-08-09 10:36:19 +0200

    Fixing Ramanujan's taxi

--- 1. Computer Science: Programming with a Purpose/1.10. Ramanujan s Taxi/ramanujan_s_taxi.java
+++ 1. Computer Science: Programming with a Purpose/1.10. Ramanujan s Taxi/ramanujan_s_taxi.java
@@ -5,26 +5,26 @@ public class ramanujan_s_taxi {
 
         for (int a = 1; a <= Math.cbrt(n); a++)
         {
-            for (int b = 1; b <= a; b++)
+            for (int b = 1; b < a; b++)
             {
                 for (int c = 1; c <= b; c++)
                 {
-                    for (int d = 1; d <= c; d++)
+                    for (int d = 1; d < c; d++)
                     {
                         int a3  =   a * a * a;
                         int b3  =   b * b * b;
                         int c3  =   c * c * c;
                         int d3  =   d * d * d;
 
-                        if (a != c && a3 + b3 == c3 + d3){
+                        if (a3 + b3 == c3 + d3){
                             System.out.println(a3 + ", " + b3 + ", " + c3 + ", " + d3);
                             System.out.println(a + "^3 + " + b + "^3 = " + (a3 + b3) + " = " + c + "^3 + " + d + "^3");
                         }
-                        if (a != b && a3 + c3 == b3 + d3){
+                        else if (a3 + c3 == b3 + d3){
                             System.out.println(a3 + ", " + b3 + ", " + c3 + ", " + d3);
                             System.out.println(a + "^3 + " + c + "^3 = " + (a3 + c3) + " = " + b + "^3 + " + d + "^3");
                         }
-                        if (a != c && a3 + d3 == b3 + c3){
+                        else if (a3 + d3 == b3 + c3){
                             System.out.println(a3 + ", " + b3 + ", " + c3 + ", " + d3);
                             System.out.println(a + "^3 + " + d + "^3 = " + (a3 + d3) + " = " + b + "^3 + " + c + "^3");
                         }
