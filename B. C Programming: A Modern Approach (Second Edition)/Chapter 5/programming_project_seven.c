bbdb338eab066a3aec2271f4ef4b1c68c8ec1587
The Night Eel <110661010+NightEel@users.noreply.github.com>
2022-08-15 13:29:13 +0200

    Seventh programming project of the fifth chapter

---
+++ B. C Programming: A Modern Approach (Second Edition)/Chapter 5/programming_project_seven.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int first, second, third, fourth, max1, min1, max2, min2;
+
+	printf("Enter four integers: ");
+	scanf("%d%d%d%d", &first, &second, &third, &fourth);
+
+	// Comparing the two first numbers
+	if (first >= second) {max1 = first; min1 = second;}
+	else                 {max1 = second; min1 = first;}
+
+    // Comparing the two last numbers
+	if (third >= fourth) {max2 = third; min2 = fourth;}
+	else                 {max2 = fourth; min2 = third;}
+
+    // Comparing the two maximums and the two minimums
+	if (max2 > max1) max1 = max2;
+	if (min2 < min1) min1 = min2;
+
+	// Printing the results
+	printf("Largest: %d\nSmallest: %d\n", max1, min1);
+}
